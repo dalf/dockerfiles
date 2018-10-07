e5013a7a90d6ce34e216368fa7ae4634739bd8b3
Alex <alex@al-f.net>
2018-10-07 11:33:24 +0200

    nghttp2-alpine/boringssl : nghttpx 1.34.0. Does NOT compile

--- nghttp2-alpine/boringssl/build.sh
+++ nghttp2-alpine/boringssl/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 # BoringSSL
 cd /build
 git clone --depth 1 --single-branch https://boringssl.googlesource.com/boringssl
@@ -16,39 +18,6 @@ make -j $(getconf _NPROCESSORS_ONLN)
 cp /build/boringssl/build/ssl/libssl* /usr/lib
 cp /build/boringssl/build/crypto/libcrypto* /usr/lib
 
-# spdylay
-if test "$SPDYLAY_VERSION" != "DISABLED"; then
-
-  cd /build
-  
-  if test -n "$SPDYLAY_VERSION"; then
-    curl -fSL https://github.com/tatsuhiro-t/spdylay/releases/download/v${SPDYLAY_VERSION}/spdylay-${SPDYLAY_VERSION}.tar.xz -o spdylay.tar.xz
-    if [ 0 -ne $? ]; then
-	exit 1
-    fi
-    ls -l spdylay.tar.xz
-    tar xJf spdylay.tar.xz
-    if [ 0 -ne $? ]; then
-	exit 1
-    fi
-    mv spdylay-${SPDYLAY_VERSION} spdylay
-  else
-    git clone https://github.com/tatsuhiro-t/spdylay.git
-  fi
-
-  cd /build/spdylay
-
-  autoreconf -i
-  automake
-  autoconf
-  ./configure --disable-dependency-tracking\
-              --disable-examples --disable-src --disable-static\
-              --prefix=/usr
-
-  make -j $(getconf _NPROCESSORS_ONLN)
-  make install
-fi
-
 # Download nghttp2
 cd /build
 if test -n "$NGHTTP2_VERSION"; then
@@ -63,7 +32,7 @@ if test -n "$NGHTTP2_VERSION"; then
   fi
   mv nghttp2-${NGHTTP2_VERSION} nghttp2
 else
-  git clone https://github.com/nghttp2/nghttp2
+  git clone --depth 1 --single-branch https://github.com/nghttp2/nghttp2
 fi
 
 # Compile / Install nghttp2
@@ -73,7 +42,7 @@ autoreconf -i
 automake
 autoconf
 
-./configure --enable-app --with-spdylay\
+./configure --enable-app\
             --disable-dependency-tracking\
             --disable-examples --disable-static\
 	    --prefix=/usr
